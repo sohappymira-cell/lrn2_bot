ab5d5a3c145d59b728c2e778bc82eb7901d615cc
sohappymira-cell <sohappymira@gmail.com>
2026-05-11 18:22:22 +0300

    жги

--- app_termin.py
+++ app_termin.py
@@ -170,3 +170,6 @@ def index():
 @app.route('/healthcheck')
 def healthcheck():
     return "OK", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
